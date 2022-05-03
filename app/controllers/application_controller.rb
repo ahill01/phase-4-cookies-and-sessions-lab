622d997c7be9bf70e5a53685e312407150fe71ed
Allison <amh03160@gmail.com>
2022-05-03 12:27:45 -0500

    commit tree eb86b7a40f4b40cee350a0046923844c3bb1b45c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-
+  def show
+    byebug;
+    session[:page_views] ||=0
+    session[:page_views] = sessions[:page_views].to_i += 1
+  end
 end
